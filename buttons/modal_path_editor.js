c768ae52f48547e2114d7ed35b791128ba8782f0
Gregory Livingston <gregory@shapiro.com>
2023-02-02 20:38:36 -0500

    preview complete for path editor

--- buttons/modal_path_editor.js
+++ buttons/modal_path_editor.js
@@ -50,14 +50,20 @@ class Modal_Path_Editor extends Modal
         this.x_input = document.createElement('input');
         this.x_input.setAttribute('type', 'text');
         this.x_input.setAttribute('style', 'z-index: 3;position: fixed;left: ' + left + 'px;top: ' + x_top + 'px;width: ' + width + 'px;height: ' + height + 'px;');
+        this.x_input.value = '40';
         document.getElementsByTagName('body')[0].appendChild(this.x_input);
 
         this.y_input = document.createElement('input');
         this.y_input.setAttribute('type', 'text');
         this.y_input.setAttribute('style', 'z-index: 3;position: fixed;left: ' + left + 'px;top: ' + y_top + 'px;width: ' + width + 'px;height: ' + height + 'px;');
+        this.y_input.value = 't';
         document.getElementsByTagName('body')[0].appendChild(this.y_input);
 
+        this.frame = 0;
+
     }
+
+//math.evaluate('1 + 2');
 
     close()
     {
@@ -89,6 +95,23 @@ class Modal_Path_Editor extends Modal
 
         this.canvas.context_get().beginPath();
         this.canvas.context_get().rect(this.x + this.preview_x, this.y + this.preview_y, this.preview_width, this.preview_height);
+        this.canvas.context_get().stroke();
+        this.canvas.context_get().fill();
+
+        this.frame++;
+
+        var scope = {'t': this.frame};
+
+        var x = (math.evaluate(this.x_input.value, scope) * game_info['inner_ratio'] * 0.5) + game_info['inner_offset'] + (game_info['inner_width'] * 0.5);
+        var y = math.evaluate(this.y_input.value, scope) * game_info['inner_ratio'] * 0.5;
+
+        if(y < -1 * game_info['tile_size'] || y > (game_info['inner_height'] * 0.5) + game_info['tile_size'])
+        {
+            this.frame = 0;
+        }
+
+        this.canvas.context_get().beginPath();
+        this.canvas.context_get().arc(x, y, game_info['tile_size'] * 0.25 * game_info['inner_ratio'], 0, 2 * Math.PI);
         this.canvas.context_get().stroke();
         this.canvas.context_get().fill();
 
